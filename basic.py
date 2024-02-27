db906a5e4412ace0f6c3022b6d1af8cffc7800d8
pimas12 <samip.ghimire1@gmail.com>
2024-02-27 15:39:58 +0545

    basic python

---
+++ basic.py
@@ -0,0 +1,13 @@
+# user_age =int(input("Enter your age:"))
+# if user_age>19:
+#     print("you can go")
+# else:
+#     print("you cant go")
+
+
+# write a Python that calculates the area of a circle based on the radius entered by the user 
+
+
+radius=float(input("enter your radius"))
+area=22/7*radius**2
+print(f"the area is{area}")
